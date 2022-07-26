293fcfae320b7aab297957221cf758b34fa67848
yayakame <83417264+yayakateacup@users.noreply.github.com>
2022-07-26 00:00:17 -0300

    removed some global states, added parameters

--- headers/main.h
+++ headers/main.h
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <windows.h>
 
-void find_and_delete_terraria_files(char *initial_folder_path);
+void find_and_delete_terraria_files(char *initial_folder_path, int current_depth);
 void change_console_color(HANDLE console_handle, WORD colors);
-void iterate_through_folder(HANDLE folder_hwnd, char *path_folder);
-void process_file_information();
+void iterate_through_folder(HANDLE folder_hwnd, char *path_folder, int c_depth);
+void process_file_information(char *path);
--- main.c
+++ main.c
@@ -13,11 +13,8 @@
 char original_header[] =
     {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52, 0x00, 0x00};
 
-int current_depth = -1;
 int max_depth = INT_MAX;
 WIN32_FIND_DATA find_data;
-
-char sub_f_path[512];
 
 char path_output_file[22];
 FILE *paths_file;
@@ -33,12 +30,15 @@ int main()
     old_console_colors = temp.wAttributes;
 
     sprintf(path_output_file, "paths-%lu.txt", GetTickCount64());
+    
     paths_file = fopen(path_output_file, "ab+");
     printf("input the initial folder path : ");
+    
     char buffer[512];
     gets(buffer);
 
-    find_and_delete_terraria_files(buffer);
+    int depth = 0;
+    find_and_delete_terraria_files(buffer, depth);
 
     fclose(paths_file);
 
@@ -47,13 +47,12 @@ int main()
     gets(buffer);
 }
 
-void find_and_delete_terraria_files(char *initial_folder_path)
+void find_and_delete_terraria_files(char *initial_folder_path, int current_depth)
 {
     if (current_depth >= max_depth)
     {
         return;
     }
-    current_depth += 1;
 
     char full_p_w_astrk[512];
     sprintf(full_p_w_astrk, "%s\\*", initial_folder_path);
@@ -69,13 +68,12 @@ void find_and_delete_terraria_files(char *initial_folder_path)
         return;
     }
 
-    iterate_through_folder(folder_hwnd, initial_folder_path);
+    iterate_through_folder(folder_hwnd, initial_folder_path, current_depth);
 
     FindClose(folder_hwnd);
-    current_depth -= 1;
 }
 
-void iterate_through_folder(HANDLE folder_hwnd, char *path_folder)
+void iterate_through_folder(HANDLE folder_hwnd, char *path_folder, int c_depth)
 {
     do
     {
@@ -85,34 +83,35 @@ void iterate_through_folder(HANDLE folder_hwnd, char *path_folder)
             continue;
         }
 
+        char sub_f_path[512];
         sprintf(sub_f_path, "%s\\%s", path_folder, find_data.cFileName);
         if (find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
         {
-            find_and_delete_terraria_files(sub_f_path);
+            find_and_delete_terraria_files(sub_f_path, c_depth + 1);
         }
         else
         {
-            process_file_information();
+            process_file_information(sub_f_path);
         }
     } while (FindNextFileA(folder_hwnd, &find_data));
 }
 
-void process_file_information()
+void process_file_information(char *path)
 {
     int header_size_b = sizeof(original_header);
     char file_header[header_size_b];
 
-    FILE *file_being_read = fopen(sub_f_path, "rb");
+    FILE *file_being_read = fopen(path, "rb");
     fread(file_header, 1, header_size_b, file_being_read);
     fclose(file_being_read);
-    
+
     if (memcmp(file_header, original_header, header_size_b) == 0)
     {
         change_console_color(console_hwnd, F_GREEN);
         printf("%s has a valid header!\n", find_data.cFileName);
 
         char file_contents[512];
-        sprintf(file_contents, "%s\n", sub_f_path);
+        sprintf(file_contents, "%s\n", path);
         fseek(paths_file, 0, SEEK_END);
         fwrite(file_contents, 1, strlen(file_contents), paths_file);
     }
